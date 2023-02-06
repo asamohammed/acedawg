1cb3f7b115f21baf1eeb54571e235cae882a4baa
Asa <asa.mohammed22@gmail.com>
2023-02-05 19:12:24 -0500

    This works

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const app = express()
 
 const PORT = process.env.PORT || 3000 
 
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-    res.send("hi")
+    const ma = "woraaaaddld";
+    res.render("index", {math:ma});
+})
+
+app.get("/mark69", (req, res) => {
+    // connect to db and get the values add one to it and save it
+})
+
+app.get("/asa69", (req, res) => {
+    
 })
 
 app.listen(PORT)
