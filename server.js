ba48628fd949074ec4977b9dc783bfd9280b68bf
Asa <asa.mohammed22@gmail.com>
2023-02-05 19:43:56 -0500

    adds db and env

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 const express = require("express")
-const app = express()
+const { Pool } = require("pg");
 
+const app = express()
 const PORT = process.env.PORT || 3000 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    const ma = "woraaaaddld";
-    res.render("index", {math:ma});
-})
-
-app.get("/mark69", (req, res) => {
-    // connect to db and get the values add one to it and save it
-})
-
-app.get("/asa69", (req, res) => {
+app.get("/", async (req, res) => {
+    const client = await pool.connect();
+	result = await client.query(`SELECT * FROM score;`);
+	result = result.rows;
     
+    
+    const dbAsaScore = "";
+    const dbMarkScore = "";
+
+    // check the status code before rendering and give it a delay
+
+    res.render("index", {asascore:dbAsaScore, markscore:dbMarkScore});
+})
+
+app.get("/mark69", async (req, res) => {
+    // connect to db and get the values add one to it and save it
+    const client = await pool.connect();
+	result = await client.query(`SELECT * FROM suggest;`);
+	result = result.rows;
+
+    
+    res.send(result);
+	client.release();
+})
+
+app.get("/asa69", async (req, res) => {
+    
+    // redirect back to normal page
 })
 
 app.listen(PORT)
